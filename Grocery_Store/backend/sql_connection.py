acabc791cff4a322eed5441050b5ab6f2c1adea1
sutharnarendra <146803457+sutharnarendra@users.noreply.github.com>
2025-05-20 19:17:25 +0530

    Delete Grocery_Store directory

--- Grocery_Store/backend/sql_connection.py
+++
@@ -1,8 +0,0 @@
-__cnx=None
-import mysql.connector
-
-def get_sql_connection():
-    global __cnx
-    if __cnx is None:
-        __cnx=mysql.connector.connect(host='localhost',user='root',password='root',database='GS')
-    return __cnx
